fc303a1a053d7a1ef8dbb4c6f3f9aa29d980e4dc
zuihaojp <kanacoconatu@gmail.com>
2024-11-21 19:03:27 +0900

    Update cameras.js

--- cameras.js
+++ cameras.js
@@ -1,5 +1,23 @@
-[
-  { "id": 1, "lat": 35.6895, "lng": 139.6917, "name": "Tokyo Camera 1", "url": "http://example.com/stream1" },
-  { "id": 2, "lat": 35.6897, "lng": 139.6921, "name": "Tokyo Camera 2", "url": "http://example.com/stream2" },
-  { "id": 3, "lat": 35.6900, "lng": 139.6930, "name": "Tokyo Camera 3", "url": "http://example.com/stream3" }
-]
+const cameras = [
+  {
+    id: 1,
+    lat: 35.6895, // 緯度
+    lng: 139.6917, // 経度
+    name: "東京・新宿交差点 (Insecam)",
+    url: "http://www.insecam.org/en/view/123456/" // Insecamのカメラリンク
+  },
+  {
+    id: 2,
+    lat: 34.6937, // 緯度
+    lng: 135.5023, // 経度
+    name: "大阪・道頓堀 (Insecam)",
+    url: "http://www.insecam.org/en/view/234567/" // Insecamのカメラリンク
+  },
+  {
+    id: 3,
+    lat: 35.0116, // 緯度
+    lng: 135.7681, // 経度
+    name: "京都・鴨川 (Insecam)",
+    url: "http://www.insecam.org/en/view/345678/" // Insecamのカメラリンク
+  }
+];
