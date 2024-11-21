008c3eee2f550c499821e1169b4b32cce57f89e5
zuihaojp <kanacoconatu@gmail.com>
2024-11-21 18:43:31 +0900

    Create cameras.js
    カメラデータ

---
+++ cameras.js
@@ -0,0 +1,5 @@
+[
+  { "id": 1, "lat": 35.6895, "lng": 139.6917, "name": "Tokyo Camera 1", "url": "http://example.com/stream1" },
+  { "id": 2, "lat": 35.6897, "lng": 139.6921, "name": "Tokyo Camera 2", "url": "http://example.com/stream2" },
+  { "id": 3, "lat": 35.6900, "lng": 139.6930, "name": "Tokyo Camera 3", "url": "http://example.com/stream3" }
+]
